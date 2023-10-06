30dc9c4a8da1452b66ade9ea8f7c97f783c88de5
Sanjaya Budhathoki <sanju@Sanjayas-MacBook-Pro.local>
2023-10-06 14:18:06 -0400

    Initial Commit

---
+++ components/Login.tsx
@@ -0,0 +1,12 @@
+const LoginButton = () => {
+  return (
+    <a
+      href="api/auth/login"
+      className="bg-blue-500 hover:bg-blue-600 text-black font-bold py-2 px-4 rounded"
+    >
+      Log In
+    </a>
+  );
+};
+
+export default LoginButton;
